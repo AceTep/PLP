2322b9cb8b1441839bf868d22e55041d30de21c8
AceTep <72736849+AceTep@users.noreply.github.com>
2023-12-01 17:14:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // You can add code here to handle form submission, like sending an email or storing data.
+    // For this example, let's just display a message.
+    alert('Form submitted!'); 
+  });
+  
